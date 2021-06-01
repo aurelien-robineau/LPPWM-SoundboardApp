b50de7341f4f8cd7723dcc45a3b231c68e73d329
Aurélien <robineau.aure@gmail.com>
2021-06-01 13:47:27 +0200

    Add redux persist

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import MainNavigator from './navigators/MainNavigator'
 
 import config from './config'
-import store from './store/index'
+import store, { persistor } from './store/index'
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<StatusBar backgroundColor={config.colors.main} barStyle='light-content' />
-			<NavigationContainer>
-				<MainNavigator />
-			</NavigationContainer>
+			<PersistGate loading={null} persistor={persistor}>
+				<StatusBar backgroundColor={config.colors.main} barStyle='light-content' />
+				<NavigationContainer>
+					<MainNavigator />
+				</NavigationContainer>
+			</PersistGate>
 		</Provider>
 	)
 }
--- store/index.js
+++ store/index.js
@@ -1,13 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
-
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import samplersReducer from './samplersSlice'
 import libraryReducer from './librarySlice'
 
+const libraryPersistConfig = {
+	key: 'library',
+	version: 1,
+	storage: AsyncStorage,
+	stateReconciler: hardSet,
+}
+
+
+const samplersPersistConfig = {
+	key: 'samplers',
+	version: 1,
+	storage: AsyncStorage,
+	stateReconciler: hardSet,
+}
+
+const persistedLibraryReducer = persistReducer(libraryPersistConfig, libraryReducer)
+const persistedSamplersReducer = persistReducer(samplersPersistConfig, samplersReducer)
+
 const store = configureStore({
 	reducer: {
-		samplers: samplersReducer,
-		library: libraryReducer
-	}
+		library: persistedLibraryReducer,
+		samplers: persistedSamplersReducer
+	},
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+		},
+	}),
 })
+
+export const persistor = persistStore(store)
 
 export default store
